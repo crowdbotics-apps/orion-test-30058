079090a73093c70a08d88a8573de73aeb2b49ab3
Crowdbotics <team@crowdbotics.com>
2021-08-23 22:39:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "orion_test_30058Identifier",
+  url: "https://orion_test_30058Identifier.botics.co",
+  api: "https://orion_test_30058Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
